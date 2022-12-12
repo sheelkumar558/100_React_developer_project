98c7868fbb5986c47f2e3a448464e06eb58cd608
sheelkumar558 <sheelkumar558@gmail.com>
2022-12-12 20:39:42 +0530

    simple slide

---
+++ 05-Image-Slider/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import Slide from "./components/Slide"
+
+function App() {
+  return (
+    <main>
+      {/*<h1>Images Slider</h1>*/}
+      <Slide />
+    </main>
+  )
+}
+
+export default App
