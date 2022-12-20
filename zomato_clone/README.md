957db865beb264bc6b5e8fb7b5e173f72219dc5b
sheelkumar558 <sheelkumar558@gmail.com>
2022-12-20 16:56:17 +0530

    zomato app

---
+++ zomato_clone/README.md
@@ -0,0 +1,5 @@
+### zomato clone
+
+### live link
+
+###
---
+++ zomato_clone/src/components/common/footer/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="absolute-center max-width">Zomato clone</div>;
+};
+
+export default Footer;
